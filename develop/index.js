df78985a229462ab0988da2c4743829e8c915d5a
Peter Sammon <petersammon2@gmail.com>
2020-05-22 20:22:45 -0400

    psuedocoding steps to take

---
+++ develop/index.js
@@ -0,0 +1,19 @@
+const questions = [];
+
+function writeToFile(fileName, data) {}
+
+function init() {
+	// get input from users
+	// -- use inquirer
+	// after we get input from users, make api call to github
+	// -- use axios
+	// -- the rest of this should inside the axios then block
+	// collect additional data from github response
+	// send all of the data to the generateData markdown function to collect the formatted markdown
+	// user the generateMarkdown function
+	// const markdown = generateMarkdown(data, response)
+	// create the markdown file with the formatted markdown
+	// use markdown
+}
+
+init();
